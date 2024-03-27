2d68e5ed87990eaa1651af9d2e2353f0b1f8262e
Journey99 <g48993818@gmail.com>
2024-03-27 21:52:14 +0900

    modify code

--- src/models/Image Classification/VGGNet.py
+++ src/models/Image Classification/VGGNet.py
@@ -5,21 +5,19 @@ from torch import optim
 from torchsummary import summary
 
 
-class VGG(nn.Module):
+class VGG16(nn.Module):
     def __init__(self, base_dim, num_classes=10):
-        super(VGG, self).__init__()
+        super(VGG16, self).__init__()
         self.feature = nn.Sequential(
-            conv_2_block(3,base_dim), #64
-            conv_2_block(base_dim,2*base_dim), #128
-            conv_3_block(2*base_dim,4*base_dim), #256
-            conv_3_block(4*base_dim,8*base_dim), #512
-            conv_3_block(8*base_dim,8*base_dim), #512        
+            conv_2_block(3,base_dim), # 3x224x224 -> 64x112x112
+            conv_2_block(base_dim,2*base_dim), # 64x112x112 -> 128x56x56
+            conv_3_block(2*base_dim,4*base_dim), # 128x56x56 -> 256x28x28
+            conv_3_block(4*base_dim,8*base_dim), # 256x28x28 -> 512x14x14
+            conv_3_block(8*base_dim,8*base_dim), # 512x14x14 -> 512x7x7   
         )
         self.fc_layer = nn.Sequential(
-            # CIFAR10은 크기가 32x32이므로 
-            nn.Linear(8*base_dim*1*1, 4096),
-            # IMAGENET이면 224x224이므로
-            # nn.Linear(8*base_dim*7*7, 4096),
+            # IMAGENET : 224x224이므로
+            nn.Linear(512*7*7, 4096),
             nn.ReLU(True),
             nn.Dropout(),
             nn.Linear(4096, 1000),
@@ -31,7 +29,8 @@ class VGG(nn.Module):
     def forward(self, x):
         x = self.feature(x)
         #print(x.shape)
-        x = x.view(x.size(0), -1)
+        # x = x.view(x.size(0), -1)
+        x = torch.flatten(x, 1) # 1차원으로 펴줌
         #print(x.shape)
         x = self.fc_layer(x)
         return x
@@ -64,5 +63,6 @@ def conv_3_block(in_dim,out_dim):
 if __name__ == "__main__":
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     print(device)
-    model = VGG(base_dim=64).to(device)
-    print(summary(model, input_size=(3, 224, 224).to(device)))
+    model = VGG16(base_dim=64).to(device)
+    # print(model)
+    print(summary(model, input_size=(3, 224, 224)))
